b8519bcb8db6dc0223922b42bcec058c214adfe7
Scordo_cp <SND\Scordo_cp>
2012-09-12 15:53:47 +0000

    Implemented workaround for a bug in roslyn compiler. (event documentation is not correctly retrieved by roslyn compiler)

--- Main/CDCP/Processing/EventSymbolProcessor.cs
+++ Main/CDCP/Processing/EventSymbolProcessor.cs
@@ -1,6 +1,8 @@
-﻿using CDCP.Configuration;
+﻿using System.Reflection;
+using CDCP.Configuration;
 using Roslyn.Compilers;
 using Roslyn.Compilers.CSharp;
+using System.Linq;
 
 namespace CDCP.Processing
 {
@@ -23,10 +25,55 @@ namespace CDCP.Processing
 			if (!AnyVisibilityMatches(symbol.DeclaredAccessibility, config.VisibilitiesToCheck) && symbol.CanBeReferencedByName)
                 return;
 
-            DocumentationComment documentation = symbol.GetDocumentationComment();
+			DocumentationComment documentation = GetDocumentationComment(symbol);
 
             if (config.SummaryDocumentationRequired && string.IsNullOrWhiteSpace(documentation.SummaryTextOpt))
                 violationReporter.Report(ViolationFromSymbol(ViolationMessage.MissingSummaryDocumentation, symbol));
         }
+
+		private static DocumentationComment GetDocumentationComment(Symbol symbol)
+		{
+			// return symbol.GetDocumentationComment(); // this is buggy in roslyn
+
+
+			// this is a dirty workaround for a bug in roslyn
+			try
+			{
+				SyntaxNode currentNode = GetCurrentSyntaxNode(symbol);
+				SyntaxNode realEventNode = GetEventSyntaxNode(currentNode);
+				DocumentationCommentSyntax documentationSyntax = (DocumentationCommentSyntax)realEventNode.GetLeadingTrivia().First(t => t.Kind == SyntaxKind.DocumentationComment).GetStructure();
+
+				return DocumentationComment.FromXmlFragment(documentationSyntax.GetInteriorXml());
+			}
+			catch
+			{
+				return symbol.GetDocumentationComment();
+			}
+		}
+
+		private static SyntaxNode GetEventSyntaxNode(SyntaxNode currentNode)
+		{
+			while (currentNode != null)
+			{
+				if (currentNode.Kind == SyntaxKind.EventFieldDeclaration || currentNode.Kind == SyntaxKind.EventDeclaration)
+					return currentNode;
+
+				currentNode = currentNode.Parent;
+			}
+
+			return null;
+		}
+
+		private static SyntaxNode GetCurrentSyntaxNode(Symbol symbol)
+		{
+			try
+			{
+				return (SyntaxNode)symbol.GetType().GetProperty("SyntaxNode", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(symbol, null);
+			}
+			catch
+			{
+				return null;
+			}
+		}
     }
 }
