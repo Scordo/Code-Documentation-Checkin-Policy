fc2d313f1cf29f96832952b21320be2620e4c658
Scordo_cp <Scordo@users.noreply.github.com>
2015-05-12 05:15:25 +0000

    Fixed deployment process

--- CDCP/Deployment/CreateDebugRelease.bat
+++ CDCP/Deployment/CreateDebugRelease.bat
@@ -1,3 +1,3 @@
 ﻿
-"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" Deployment.target
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" Deployment.target
 pause
--- CDCP/Deployment/CreateProdRelease.bat
+++ CDCP/Deployment/CreateProdRelease.bat
@@ -1,3 +1,3 @@
 ﻿
-"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" Deployment.target /p:Configuration=Release;IncludeVersionInfo=true
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" Deployment.target /p:Configuration=Release;IncludeVersionInfo=true
 pause
